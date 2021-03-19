9f0128f54fb6a4f81b841b3da2ef7805090e9724
FaridFreestyle <faridfreestyle11@gmail.com>
2021-03-19 14:59:38 -0400

    up

--- src/components/ImageLikeForm/ImageLikeForm.js
+++ src/components/ImageLikeForm/ImageLikeForm.js
@@ -8,7 +8,7 @@ return (
 			{'This Magic Brain will detect faces in your pictures. Get it try.'}
 		</p>
 		<p className='f4'>
-			{'Put a image link ex: https://www.spsp.org/sites/default/files/fisher-single-people-social-groups.jpg'}
+			{'Put an image link ex: https://www.spsp.org/sites/default/files/fisher-single-people-social-groups.jpg'}
 		</p>
 		<div className='center'>
 			<div className=' form center pa4 br3 shadow-5'>
